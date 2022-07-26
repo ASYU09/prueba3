e73cf37fc63915766059b3c797ecc6c63b3f9743
linent <andersoneduardoop@ufps.edu.co>
2022-07-26 15:02:51 -0500

    prueba terminada

--- app.js
+++ app.js
@@ -1,65 +1,102 @@
 
 let asisgnaturaArr = []
 let datos = document.querySelector("#table");
-datos.style.display = "none";
 
-let formulario= document.querySelector('#formulario')
-formulario.addEventlistnerListener('submit', function(){
 
-  const asig={
+let formulario = document.querySelector('#formulario')
+formulario.addEventListener('submit', function () {
 
-    asignatura: document.getElementById('#as').value,
-    primeraN: document.getElementById('#n1').value,
-    segundaN: document.getElementById('#n2').value,
-    terceraN: document.getElementById('#n3').value,
-    examenF: document.getElementById('#ex').value,
-  } 
+  const asig = {
+
+    asignatura: document.getElementById('as').value,
+    primeraN: document.getElementById('p1').value,
+    segundaN: document.getElementById('p2').value,
+    terceraN: document.getElementById('p3').value,
+    examenF: document.getElementById('ex').value,
+  }
+
+
   asisgnaturaArr.push(asig)
-  definitiva()
+  definitivaFinal()
+  cambiarColor()
+  
 
   let tabla = `
-    <thead class="thead-dark">
+          <thead>
             <tr>
               <th scope="col">asignatura</th>
               <th scope="col">primeraN</th>
               <th scope="col">segundaN</th>
               <th scope="col">terceraN</th>
               <th scope="col">Examen Final</th>
-              <th scope="col">examenF</th>
+              <th scope="col">Definitiva</th>
             </tr>
-    </thead> 
+            </thead>
+      <tbody>
     `
+
+
+  asisgnaturaArr.forEach((asignar) => {
+
+    tabla += `
+      <tr>
+        <td id="as">${asignar.asignatura}</td>
+        <td id="p1">${asignar.primeraN}</td>
+        <td id="p2">${asignar.segundaN}</td>
+        <td id="p3">${asignar.terceraN}</td>
+        <td id="ex">${asignar.examenF}</td>
+        <td style="background-color: ${asignar.color};">${asignar.definitiva}</td>
+
+      </tr>
+      </tbody>
+      `
+
+    let tabla2 = document.getElementById('table')
+    tabla2.innerHTML = tabla
+  })
+})
+
+function cambiarColor(){
     
-})
+    
+    
+  asisgnaturaArr.map(element=>{
 
-materias.forEach((asignar)=>{
-
-  tabla += `
-  <tr>
-    <td id="as">${asignar.asignatura}</td>
-    <td id="n1">${asignar.primeraN}</td>
-    <td id="n2">${asignar.segundaN}</td>
-    <td id="n3">${asignar.terceraN}</td>
-    <td id="ex">${asignar.examenF}</td>
-  </tr>`
-
-  let tabla= document.getElementById('tab')
-  tabla.innerHTML= tabla
-})
-
-
-datos.style.display = "block";
-
-function definitiva(l){
-  for (let i = 0; i < notaFinal.length; i++) {
+      if(element.definitiva>0.0&&element.definitiva<=2.9){
+  
+          element.color="red"
+      }else if(element.definitiva>=3.0&&element.definitiva<=3.9){
+  
+          element.color="yellow"
+      }else if(element.definitiva>=4.0&&element.definitiva<=5.0){
+  
+          element.color="green"
+      }      
       
-
-    notaFinal[i].def= ( ((notaFinal[i].p1+notaFinal[i].p2+notaFinal[i].p3)/3)*0.7+(notaFinal[i].examenF*0.3))
-
-  }
-
-  element.definitiva= definitiva
+      
+  })
 }
 
 
+function definitivaFinal() {
+
+  asisgnaturaArr.map(element => {
+
+    let defin = (((Number(element.primeraN) + Number(element.segundaN) + Number(element.terceraN))/3)*0.7+(element.examenF * 0.3))
+  
+    if(defin>=2.95&&defin<3){
+            
+      defin=3
+    }
+    element.definitiva = defin
+
+  })
+
+}
+
+
+
+
+
+  
 
